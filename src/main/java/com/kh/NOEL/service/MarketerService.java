ecef7ec335e6ea8f38d835bd0e5b871fd325f0ef
jooyoun lee <115621310+zwzwzw123@users.noreply.github.com>
2023-05-11 15:32:32 +0900

    [feat] 판매자 회원가입 비밀번호 암호화

--- src/main/java/com/kh/NOEL/service/MarketerService.java
+++ src/main/java/com/kh/NOEL/service/MarketerService.java
@@ -6,6 +6,7 @@ import com.kh.NOEL.dto.MarketerDto;
 import com.kh.NOEL.mapper.MarketerMapper;
 import com.kh.NOEL.repository.MarketerRespository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
@@ -16,8 +17,14 @@ public class MarketerService {
     @Autowired
     private MarketerRespository marketerRespository;
 
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
     public MarketerDto joinMarketer(MarketerDto marketerDto) {
         Marketer marketer = MarketerMapper.convertToModel(marketerDto);
+        String rawPw = marketerDto.getMarketerPw();
+        String encPw = encoder.encode(rawPw);
+        marketer.setMarketerPw(encPw);
         marketer.setMarketerAuth(MarketerAuth.승인확인중);
         this.marketerRespository.save(marketer);
         return MarketerMapper.converToDto(marketer);
